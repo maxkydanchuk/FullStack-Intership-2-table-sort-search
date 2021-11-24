a9b927d7ceeb124d6d45ad76445870e1c6164b46
Max Kydanchuk <maxym.kydanchuk@gmail.com>
2021-11-24 13:01:32 +0200

    refactored code using  class component

--- index.js
+++ index.js
@@ -1,7 +1,72 @@
+"use strict";
 let dataCopy = [];
+let isAsc = false;
 
+function quickSort(dataset, property, orderIsAsc) {
+    if (dataset.length <= 1) {
+        return dataset;
+    }
 
-const getResource = async function (url) {
+    let middleIndex = Math.floor(dataset.length / 2);
+    let middleElement = dataset[middleIndex][property];
+    let left = [];
+    let right = [];
+
+    for (let i = 0; i < dataset.length; i++) {
+        if (i === middleIndex) {
+            continue;
+        }
+
+        if (orderIsAsc) {
+            if (dataset[i][property] < middleElement) {
+                left.push(dataset[i]);
+            } else {
+                right.push(dataset[i]);
+            }
+        }
+        if (!orderIsAsc) {
+            if (dataset[i][property] > middleElement) {
+                left.push(dataset[i]);
+            } else {
+                right.push(dataset[i]);
+            }
+        }
+    }
+    return [
+        ...quickSort(left, property, orderIsAsc),
+        dataset[middleIndex],
+        ...quickSort(right, property, orderIsAsc)
+    ];
+}
+
+class RowCard {
+    constructor(year, category, firstname, lastname, fullname, parentSelector) {
+        this.year = year;
+        this.category = category;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.fullname = fullname;
+        this.parent = document.querySelector(parentSelector);
+    }
+
+    render() {
+        const element = document.createElement("div");
+        element.classList.add("table__row");
+        element.innerHTML = `
+         <span class="table__row_checkbox">
+            <input type="checkbox">
+        </span>
+        <span class="table__header_year">${this.year}</span>
+        <span class="table__row_category">${this.category}</span>
+        <span class="table__row_firstname">${this.firstname}</span>
+        <span class="table__row_lastname">${this.lastname}</span>
+        <span class="table__row_fullname">${this.fullname}</span>
+        `;
+        this.parent.append(element);
+    }
+}
+
+const getResource = async (url) => {
     const res = await fetch(url);
 
     if (!res.ok) {
@@ -10,50 +75,41 @@ const getResource = async function (url) {
     return await res.json();
 };
 
-const template = document.createElement('template');
-template.innerHTML = `
-    <div class="table__row">
-        <slot class="table__row_year" name="item__year">Year</slot>
-        <slot class="table__row_category" name="item__category">category</slot>
-        <slot class="table__row_firstname" name="item__firstname">firstname</slot>
-        <slot class="table__row_lastname" name="item__surname">lastname</slot>
-        <slot class="table__row_fullname" name="item__fullname">fullname</slot>
-    </div>
-`
+getResource("http://api.nobelprize.org/v1/prize.json").then((data) => {
+    dataCopy = data.prizes;
+    renderData(dataCopy);
+});
 
+const yearBtn = document.querySelector(".table__header_year");
 
-class NobelPrize extends HTMLElement {
-    constructor() {
-        super();
-        const shadowRoot = this.attachShadow({ mode: 'closed' });
-        shadowRoot.appendChild(template.content.cloneNode(true));
-        getResource('http://api.nobelprize.org/v1/prize.json')
-            .then((data) => {
-                dataCopy = data.prizes;
-                dataCopy.forEach(({ year, category, laureates }) => {
-                    if (laureates) {
-                        laureates.forEach(({ firstname, surname }) => {
-                            if (surname === undefined) {
-                                surname = '';
-                            }
-                            if (firstname === undefined) {
-                                firstname = '';
-                            }
+yearBtn.addEventListener("click", () => {
+    const rowWrapper = document.querySelector(".table__row_wrapper");
+    rowWrapper.innerHTML = "";
+    console.log(1);
+    isAsc = !isAsc;
+    const sortedData = quickSort(dataCopy, "year", isAsc);
+    renderData(sortedData);
+});
 
-                            const content = template.content.cloneNode(true);
-                            content.querySelector('slot[name="item__year"]').innerHTML = year;
-                            content.querySelector('slot[name="item__category"]').innerHTML = category;
-                            content.querySelector('slot[name="item__firstname"]').innerHTML = firstname;
-                            content.querySelector('slot[name="item__surname"]').innerHTML = surname;
-                            content.querySelector('slot[name="item__fullname"]').innerHTML = firstname + ' ' + surname;
-                            shadowRoot.appendChild(content);
-                        })
-                    }
-                })
+function renderData(dataset) {
+    dataset.forEach(({ year, category, laureates }) => {
+        if (laureates) {
+            laureates.forEach(({ firstname, surname }) => {
+                if (surname === undefined) {
+                    surname = "";
+                }
+                if (firstname === undefined) {
+                    firstname = "";
+                }
+                new RowCard(
+                    year,
+                    category,
+                    firstname,
+                    surname,
+                    `${firstname}  ${surname}`,
+                    ".table__row_wrapper"
+                ).render();
             });
-    }
+        }
+    });
 }
-
-customElements.define('nobel-prize', NobelPrize)
-
-
