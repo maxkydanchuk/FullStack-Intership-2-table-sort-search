1b766eb411fcbdb89655825e4f518b97a514bd18
Max Kydanchuk <maxym.kydanchuk@gmail.com>
2021-11-29 09:52:26 +0200

    added sorting and searhing

--- index.js
+++ index.js
@@ -1,6 +1,100 @@
-"use strict";
-let dataCopy = [];
+class RowCard {
+    constructor(year, category, firstname, lastname, fullname, parentSelector) {
+        this.year = year;
+        this.category = category;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.fullname = fullname;
+        this.parent = document.querySelector(parentSelector);
+    }
+
+    render() {
+        const element = document.createElement("div");
+        element.classList.add("table__row");
+        element.innerHTML = `
+        <span class="table__header_year">${this.year}</span>
+        <span class="table__row_category">${this.category}</span>
+        <span class="table__row_firstname">${this.firstname}</span>
+        <span class="table__row_lastname">${this.lastname}</span>
+        <span class="table__row_fullname">${this.fullname}</span>
+        <hr>
+        `;
+        this.parent.append(element);
+    }
+}
+
+//
+//Adapt data
+//
+
+let dataArray = [];
+
+const adaptData = (data) => {
+    const {prizes} = data;
+    const result = [];
+
+    prizes.forEach((prize) => {
+        const {year, category, laureates} = prize;
+        if (laureates) {
+            laureates.forEach((laureat) => {
+                const {firstname, surname} = laureat;
+                const fullname = [firstname, surname].filter(Boolean).join(' ');
+                result.push({year, category, firstname, surname, fullname});
+            })
+        }
+    })
+
+    return result
+}
+
+//
+//Fetch Data
+//
+
+const getResource = async (url) => {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+        throw new Error(`Could not fetch ${url}, status ${res.status}`);
+    }
+    return await res.json();
+};
+
+getResource("http://api.nobelprize.org/v1/prize.json").then((data) => {
+    dataArray = adaptData(data);
+    renderData(dataArray);
+});
+
+
+//
+//Render data
+//
+
+
+function renderData(dataset) {
+    dataset.forEach(({year, category, firstname, surname}) => {
+        new RowCard(
+            year,
+            category,
+            firstname || '',
+            surname || '',
+            `${firstname}  ${surname}`,
+            ".table__row_wrapper"
+        ).render();
+    })
+}
+
+
+
+//
+//Sort
+//
+
+const yearBtn = document.querySelector(".table__header_year");
+const categoryBtn = document.querySelector('.table__header_category');
+const rowWrapper = document.querySelector(".table__row_wrapper");
 let isAsc = false;
+
 
 function quickSort(dataset, property, orderIsAsc) {
     if (dataset.length <= 1) {
@@ -39,76 +133,57 @@ function quickSort(dataset, property, orderIsAsc) {
     ];
 }
 
-class RowCard {
-    constructor(year, category, firstname, lastname, fullname, parentSelector) {
-        this.year = year;
-        this.category = category;
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.fullname = fullname;
-        this.parent = document.querySelector(parentSelector);
-    }
 
-    render() {
-        const element = document.createElement("div");
-        element.classList.add("table__row");
-        element.innerHTML = `
-         <span class="table__row_checkbox">
-            <input type="checkbox">
-        </span>
-        <span class="table__header_year">${this.year}</span>
-        <span class="table__row_category">${this.category}</span>
-        <span class="table__row_firstname">${this.firstname}</span>
-        <span class="table__row_lastname">${this.lastname}</span>
-        <span class="table__row_fullname">${this.fullname}</span>
-        `;
-        this.parent.append(element);
-    }
-}
-
-const getResource = async (url) => {
-    const res = await fetch(url);
-
-    if (!res.ok) {
-        throw new Error(`Could not fetch ${url}, status ${res.status}`);
-    }
-    return await res.json();
-};
-
-getResource("http://api.nobelprize.org/v1/prize.json").then((data) => {
-    dataCopy = data.prizes;
-    renderData(dataCopy);
-});
-
-const yearBtn = document.querySelector(".table__header_year");
-
-yearBtn.addEventListener("click", () => {
-    const rowWrapper = document.querySelector(".table__row_wrapper");
+function sortFunc(param) {
     rowWrapper.innerHTML = "";
     isAsc = !isAsc;
-    const sortedData = quickSort(dataCopy, "year", isAsc);
+    const sortedData = quickSort(dataArray, param, isAsc);
     renderData(sortedData);
+}
+
+yearBtn.addEventListener("click", () => {
+    sortFunc('year');
 });
 
-function renderData(dataset) {
-    dataset.forEach(({ year, category, laureates }) => {
-        if (laureates) {
-            laureates.forEach(({ firstname, surname }) => {
-                if (surname === undefined) {
-                    surname = "";
-                }
-                if (firstname === undefined) {
-                    firstname = "";
-                }
-                new RowCard(
-                    year,
-                    category,
-                    firstname,
-                    surname,
-                    `${firstname}  ${surname}`,
-                    ".table__row_wrapper"
-                ).render();
-            });
+categoryBtn.addEventListener('click', () => {
+    sortFunc('category');
+})
+
+//
+//Search
+//
+
+
+
+
+function linearSearch(dataset, property, query) {
+
+    // const regex = document.querySelector('.table__header_search').value;
+    // const reg = new RegExp(regex, 'g');
+    // console.log(reg);
+
+
+    const searchResult = [];
+    for (let i = 0; i < dataset.length; i++) {
+        if (dataset[i][property].toLowerCase().includes(query.toLocaleLowerCase())) {
+            searchResult.push(dataset[i]);
         }
-    });
+
+        // if (dataset[i][property].match(reg)) {
+        //     searchResult.push(dataset[i]);
+        // }
+    }
+    return searchResult;
 }
+
+function filterData(searchInput) {
+    rowWrapper.innerHTML = "";
+    const filteredData = linearSearch(dataArray, 'firstname', searchInput);
+    renderData(filteredData);
+}
+
+const searchBar = document.querySelector('.table__header_search');
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value;
+    filterData(searchString);
+});
